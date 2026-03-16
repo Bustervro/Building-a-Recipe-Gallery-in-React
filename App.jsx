94401306a890b0d6760d11c62fd325e08282a33b
Bustervro <126031898+Bustervro@users.noreply.github.com>
2026-03-16 03:04:26 -0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import RecipeGallery from "./RecipeGallery";
 
 function App() {
-  return (
-    <div>
-      <RecipeGallery />
-    </div>
-  );
+  return <RecipeGallery />;
 }
 
 export default App;
